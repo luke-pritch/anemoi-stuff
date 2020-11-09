dbb3665bb49153f25a70cdf1da634b210a403f8a
Luke Pritchard <luke-pritch@users.noreply.github.com>
2020-11-09 08:41:06 -0500

    Mock Up Add Person Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { Form } from "react-bootstrap";
 
 // function AddPersonModal(props) {
 //   const [show, setShow] = useState(false);
@@ -69,7 +70,35 @@ function AddPersonModal(props) {
         <Modal.Header closeButton>
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="addNewPersonForm.InputName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Ada Lovelace" />
+            </Form.Group>
+            <Form.Group controlId="addNewPersonForm.InputAge">
+              <Form.Label>Age</Form.Label>
+              <Form.Control type="text" placeholder="34" />
+            </Form.Group>
+            <Form.Group controlId="addNewPersonForm.InputColor">
+              <Form.Label>Favourite Colour</Form.Label>
+              <Form.Control type="text" placeholder="Green" />
+            </Form.Group>
+            <Form.Group controlId="addNewPersonForm.InputFood">
+              <Form.Label>Favourite Food</Form.Label>
+              <Form.Control type="text" placeholder="Spaghetti" />
+            </Form.Group>
+            <Form.Group controlId="addNewPersonForm.SelectFriends">
+              <Form.Label>Friends</Form.Label>
+              <Form.Control as="select">
+                <option>Bob</option>
+                <option>Linda</option>
+                <option>Eve</option>
+                <option>Ancient Wizard</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
